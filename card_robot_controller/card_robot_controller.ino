ea91a5664a835e74281c3b8c1e758fde6f1a5158
TTUDhanushka <liyanagedc@gmail.com>
2023-11-19 22:18:55 +0200

    External interrupts

--- card_robot_controller/card_robot_controller.ino
+++ card_robot_controller/card_robot_controller.ino
@@ -10,9 +10,10 @@ volatile bool x_pos_lim_state = true, x_neg_lim_state = true;
 volatile bool y_pos_lim_state = true, y_neg_lim_state = true;
 
 // Constants
-const int SERIAL_BAUD = 19200;
+const float SERIAL_BAUD = 19200;
 const byte X_POS_LIM_SW = 2, X_NEG_LIM_SW = 3;
 const byte Y_POS_LIM_SW = 18, Y_NEG_LIM_SW = 19;
+const byte ENABLE_MOTOR_A = 14, ENABLE_MOTOR_B = 15;
 const float F_OSC = 16000000;     // 16MHz
 
 
@@ -25,9 +26,16 @@ bool home_x_y_axes();
 bool move_X_axis(float distance);
 
 bool initialize_robot(){
+
   bool status = false;
 
+  // Set digital pins as outputs to enable motors
+  pinMode(ENABLE_MOTOR_A, OUTPUT);
+  pinMode(ENABLE_MOTOR_B, OUTPUT);
 
+  // Enable motors
+  digitalWrite(ENABLE_MOTOR_A, HIGH);
+  digitalWrite(ENABLE_MOTOR_B, HIGH);
 
   return status;
 }
@@ -56,22 +64,20 @@ void setup_uart0_rx_interrupts(){
   UCSR0B |= bit(TXEN0); 
 
   // Set baudrate registers
-  UBRR0 |= (int)ubrrValue; 
+  UBRR0 = round(ubrrValue); 
 
   // Enable interrupts
   interrupts();
 }
 
 bool WriteToSerialPort(unsigned char data){
-  while ( !( UCSR0A & (1 << UDRE0)) )
+  while (!(UCSR0A &= bit(UDRE0))) 
   {
     ;
   }
-  // while (!(UCSR0A &= bit(UDRE0))) {
-    // UDR0 = data;
-    UDR0 = 'A';
-  // }
-
+  
+  UDR0 = data;
+  
   return true;
 }
 
@@ -80,6 +86,15 @@ ISR(USART0_RX_vect){
 
 // Do something
   
+}
+
+// X-axis positive  direction limit switch interrupt.
+void x_pos_lim_interrupt(){
+  x_pos_lim_state = digitalRead(X_POS_LIM_SW);
+}
+
+void x_neg_lim_interrupt(){
+  x_neg_lim_state = digitalRead(X_NEG_LIM_SW);
 }
 
 void setup(){
@@ -92,22 +107,18 @@ void setup(){
   pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
   pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
 
-
+  // Input interrupts for limit switches.
   attachInterrupt(digitalPinToInterrupt(X_POS_LIM_SW), x_pos_lim_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(X_NEG_LIM_SW), x_neg_lim_interrupt, CHANGE);
 
+  // UART - Serial communication interrupt setup.
   setup_uart0_rx_interrupts();
 
   initialize_robot();
 }
 
-// X-axis positive  direction limit switch interrupt.
-void x_pos_lim_interrupt(){
-  bool state = digitalRead(X_POS_LIM_SW);
-  x_pos_lim_state = state;
-}
-
 void loop(){
-  if (!x_pos_lim_state){
+  if (!x_neg_lim_state){
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
   }
@@ -116,10 +127,10 @@ void loop(){
     delay(100);
   }
 
-  unsigned char transmitBuff[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+  // unsigned char transmitBuff[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 
-  for (int n = 0; n < 7; n++){
-    WriteToSerialPort(transmitBuff[n]);
-    delay(1000);
-  }
+  // for (int n = 0; n < 7; n++){
+  //   WriteToSerialPort(transmitBuff[n]);
+  //   delay(1000);
+  // }
 }
