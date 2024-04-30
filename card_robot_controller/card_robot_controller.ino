07f97d3128dd69c952782f2408161130f9514089
TTUDhanushka <liyanagedc@gmail.com>
2024-04-30 23:54:49 +0300

    Second stepper motor added
    The timer based waveform output isn't delivering expected results.

--- card_robot_controller/card_robot_controller.ino
+++ card_robot_controller/card_robot_controller.ino
@@ -3,7 +3,7 @@ Author:       Dhanushka Liyanage
 Description:  Card robot (cartesian robot) controller.
               Read ROS serial input commands and run stepper motors.
 Date:         2023-11-01
-Updated:      2024-04-28
+Updated:      2024-04-30
 */
 
 // Globale variables
@@ -11,30 +11,51 @@ volatile bool x_pos_lim_state = true, x_neg_lim_state = true;
 volatile bool y_pos_lim_state = true, y_neg_lim_state = true;
 
 volatile uint32_t _tickCount = 0;
+volatile uint16_t dividend = 10000;
+
 
 // Constants
-const float SERIAL_BAUD = 19200;
+// const float SERIAL_BAUD = 19200;
 const byte X_POS_LIM_SW = 2, X_NEG_LIM_SW = 3;
 const byte Y_POS_LIM_SW = 18, Y_NEG_LIM_SW = 19;
 const byte ENABLE_MOTOR_A = 52, ENABLE_MOTOR_B = 48;
 const byte DIR_MOTOR_A = 50, DIR_MOTOR_B = 46;
 const byte PULS_MOTOR_A = 10, PULS_MOTOR_B = 11;
 const byte ATTACHER = 6, ARM = 7;
+
+const uint16_t MAX_PPS = 5000, MIN_PPS = 400;
+const uint16_t MAX_RPM = 750, MIN_RPM = 0;
+
+enum RobotState {Idle, Initializing, Running, ShuttingDown, Error};
+RobotState robotCurrentState = Idle;
+
 const float F_OSC = 16000000;     // 16MHz
 
 
 // Function prototypes
-void setup_uart0_rx_interrupts();
+//void setup_uart0_rx_interrupts();
+void setup_timer_interrupts();
+void setup_io_ports();
+
+void initialize_robot();
+
 // void x_pos_lim_interrupt();
 
 bool WriteToSerialPort(unsigned char data);
 bool home_x_y_axes();
 bool move_X_axis(float distance);
+void set_rpm_motor_X(uint16_t speed);
+void set_rpm_motor_Y(uint16_t speed);
 
-bool initialize_robot()
-{
+void setup_io_ports(){
+  // Status indicator
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  bool status = false;
+  //Setup limit switches
+  pinMode(X_POS_LIM_SW, INPUT_PULLUP);
+  pinMode(X_NEG_LIM_SW, INPUT_PULLUP);
+  pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
+  pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
 
   // Set digital pins as outputs to enable motors
   pinMode(ENABLE_MOTOR_A, OUTPUT);
@@ -51,141 +72,108 @@ bool initialize_robot()
   // Setup eletro magnet for grabbing
   pinMode(ATTACHER, OUTPUT);
 
-  // Enable motors
-  digitalWrite(ENABLE_MOTOR_A, HIGH);
-  digitalWrite(ENABLE_MOTOR_B, HIGH);
-
-  return status;
 }
 
-bool home_x_y_axes(){
+// void setup_uart0_rx_interrupts(){
 
-  return true;
-}
+//   float ubrrValue = (F_OSC / (16 * SERIAL_BAUD)) - 1;
 
-bool move_X_axis(float distance){
+//   noInterrupts();
 
-}
+//   UCSR0B |= bit(RXCIE0); 
 
-void setup_uart0_rx_interrupts(){
+//   // Enable receiver
+//   UCSR0B |= bit(RXEN0); 
 
-  float ubrrValue = (F_OSC / (16 * SERIAL_BAUD)) - 1;
+//   // Enable transmitter
+//   UCSR0B |= bit(TXEN0); 
 
-  noInterrupts();
+//   // Set baudrate registers
+//   UBRR0 = round(ubrrValue); 
 
-  UCSR0B |= bit(RXCIE0); 
-
-  // Enable receiver
-  UCSR0B |= bit(RXEN0); 
-
-  // Enable transmitter
-  UCSR0B |= bit(TXEN0); 
-
-  // Set baudrate registers
-  UBRR0 = round(ubrrValue); 
-
-  // Enable interrupts
-  interrupts();
-}
+//   // Enable interrupts
+//   interrupts();
+// }
 
 void setup_timer_interrupts(){
   noInterrupts();
 
-  // CLear Timer Control Register
+  // Clear Timer Control Register
   TCCR1A = 0;
   TCCR1B = 0;
 
+  TCCR2A = 0;
+  TCCR2B = 0;
+
   // Timer Interrupt Mask Register
   TIMSK1 |= (1 << OCIE1A);  // Timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);  // Timer compare interrupt
 
   // Timer Interrupt Flag Register
   TIFR1 |= (0 << TOV1);
+  TIFR2 |= (0 << TOV2);
 
   // Clock source
   TCCR1B |= (1 << CS11);  // Clk / 8 from prescaler
   TCCR1B |= (1 << WGM12); //| (1 << WGM13)
 
-  // Set pin toggle for Channel A
-  TCCR1A |= (1 << COM1A0);
-  TCCR1A |= (1 << COM2A0);
+  TCCR2B |= (1 << CS21);  // Clk / 8 from prescaler
+  TCCR2B |= (1 << WGM22); //| (1 << WGM13)
 
-  OCR1A = 199;
+  // Set pin toggle for Channel A of timer 1 and 2
+  // TCCR1A |= (1 << COM1A0);     // normal operation - external pins disconnected
+  TCCR2A |= (1 << COM2A0);
+
+  OCR1A = 99;             // This will generate 10kHz clock
+  OCR2A = 99;
 
   // Enable interrupts
   interrupts();
 }
 
-bool WriteToSerialPort(unsigned char data){
-  while (!(UCSR0A &= bit(UDRE0))) 
-  {
-    ;
-  }
-  
-  UDR0 = data;
-  
-  return true;
-}
-
-// UART0 receive interrupt service routine.
-ISR(USART0_RX_vect){
-
-// Do something
-  
-}
-
-ISR(TIMER1_OVF_vect) 
+void setup()
 {
-  // Clear the interrupt flag.
-  TIFR1 |= (0 << TOV1);
-}
 
+  setup_io_ports();
 
-ISR(TIMER1_COMPA_vect) 
-{
-  // Count the number of ticks.
-  _tickCount++;
+  //setup_uart0_rx_interrupts();          // UART - Serial communication interrupt setup.
 
-  TCNT1 = 0;
-}
+  setup_timer_interrupts();             // Setup timer interrupts for stepper motor control.
 
-// X-axis positive  direction limit switch interrupt.
-void x_pos_lim_interrupt(){
-  x_pos_lim_state = digitalRead(X_POS_LIM_SW);
-}
+  // Input interrupts for limit switches.
+  attachInterrupt(digitalPinToInterrupt(X_POS_LIM_SW), x_pos_lim_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(X_NEG_LIM_SW), x_neg_lim_interrupt, CHANGE);
 
-void x_neg_lim_interrupt(){
-  x_neg_lim_state = digitalRead(X_NEG_LIM_SW);
-}
-
-void setup(){
-  // Status indicator
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  //Setup limit switches
-  pinMode(X_POS_LIM_SW, INPUT_PULLUP);
-  pinMode(X_NEG_LIM_SW, INPUT_PULLUP);
-  pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
-  pinMode(Y_POS_LIM_SW, INPUT_PULLUP);
-
-  // // Input interrupts for limit switches.
-  // attachInterrupt(digitalPinToInterrupt(X_POS_LIM_SW), x_pos_lim_interrupt, CHANGE);
-  // attachInterrupt(digitalPinToInterrupt(X_NEG_LIM_SW), x_neg_lim_interrupt, CHANGE);
-
-  // UART - Serial communication interrupt setup.
-  setup_uart0_rx_interrupts();
-
-  setup_timer_interrupts();
-
-  initialize_robot();
-
-  digitalWrite(DIR_MOTOR_A, LOW);
-  digitalWrite(DIR_MOTOR_B, LOW);
-
+  Serial.begin(9600);
 }
 
 void loop(){
 
-  //Serial.println("Running");
+  // State machine
+  switch(robotCurrentState){
+
+    case Idle:
+      // Do nothing and switch off motors.
+      //digitalWrite(PULS_MOTOR_A, LOW);
+      //digitalWrite(PULS_MOTOR_B, LOW);
+
+      //digitalWrite(DIR_MOTOR_A, HIGH);
+      //digitalWrite(DIR_MOTOR_B, HIGH);
+
+      //Serial.println("Idle");
+
+      set_rpm_motor_X(160);
+      set_rpm_motor_Y(160);
+
+      break;
+
+    case Initializing:
+
+      initialize_robot();
+      Serial.println("Initializing");
+      break;
+
+  }
 
   // RUN magnet
   //analogWrite(ATTACHER, 127);
@@ -201,12 +189,114 @@ void loop(){
     delay(100);
   }
 
-  // unsigned char transmitBuff[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+}
 
-  // for (int n = 0; n < 7; n++){
-  //   WriteToSerialPort(transmitBuff[n]);
-  //   delay(1000);
-  // }
+void initialize_robot(){
+
+  // Enable motors
+  digitalWrite(ENABLE_MOTOR_A, HIGH);
+  digitalWrite(ENABLE_MOTOR_B, HIGH);
+
+  // Rotation directions -> Must be set in
+  digitalWrite(DIR_MOTOR_A, LOW);
+  digitalWrite(DIR_MOTOR_B, LOW);
+
+}
+
+// bool WriteToSerialPort(unsigned char data){
+//   while (!(UCSR0A &= bit(UDRE0))) 
+//   {
+//     ;
+//   }
+  
+//   UDR0 = data;
+  
+//   return true;
+// }
+
+// // UART0 receive interrupt service routine.
+// ISR(USART0_RX_vect){
+
+// // Do something
+  
+// }
+
+bool home_x_y_axes(){
+
+  return true;
+}
+
+bool move_X_axis(float distance){
+
+}
+
+
+ISR(TIMER1_OVF_vect) 
+{
+  // Clear the interrupt flag.
+  TIFR1 |= (0 << TOV1);
+}
+
+ISR(TIMER2_OVF_vect) 
+{
+  // Clear the interrupt flag.
+  TIFR2 |= (0 << TOV2);
+}
+
+ISR(TIMER1_COMPA_vect) 
+{
+  noInterrupts();
+
+  // Count the number of ticks.
+  _tickCount++;
+
+  TCNT1 = 0;
+
+  interrupts();
+}
+
+ISR(TIMER2_COMPA_vect) 
+{
+  // Count the number of ticks.
+  //_tickCount++;
+
+  TCNT2 = 0;
+}
+
+// X-axis positive  direction limit switch interrupt.
+void x_pos_lim_interrupt(){
+  x_pos_lim_state = digitalRead(X_POS_LIM_SW);
+}
+
+void x_neg_lim_interrupt(){
+  x_neg_lim_state = digitalRead(X_NEG_LIM_SW);
+}
+
+void set_rpm_motor_X(uint16_t speed)
+{
+  if (speed == 0){
+
+  }
+  int pps = map(speed, MIN_RPM, MAX_RPM, MIN_PPS, MAX_PPS);
+  Serial.println("Motor 1 PPS");
+    Serial.println(pps, DEC);
+
+  uint16_t OCR1A_setpoint = (uint16_t) (1000000 / (pps)) - 1;
+
+ OCR1A_setpoint;
+  Serial.println("Motor 1");
+  Serial.println(OCR1A_setpoint, DEC);
+}
+
+void set_rpm_motor_Y(uint16_t speed)
+{
+  int pps = map(speed, MIN_RPM, MAX_RPM, MIN_PPS, MAX_PPS);
+
+  uint16_t OCR2A_setpoint = (uint16_t) (1000000 / (pps)) - 1;
+
+  OCR2A = OCR2A_setpoint;
+  Serial.println("Motor 2");
+  Serial.println(OCR2A_setpoint);
 }
 
 // #include <Servo.h>
