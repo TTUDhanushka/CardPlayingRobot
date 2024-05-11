ea4f0a5d2900c231e60c47e6edc4e81206b3b470
TTUDhanushka <liyanagedc@gmail.com>
2024-05-11 23:45:44 +0300

    lib update

--- card_robot_controller/tb6600_stepper.cpp
+++ card_robot_controller/tb6600_stepper.cpp
@@ -8,6 +8,10 @@ uint16_t set_speed_value;
 volatile double _tickCount = 0;
 uint8_t servo_pin;
 
+uint8_t stepperCount = 0;
+
+static stepper_t setppers[MAX_STEPPERS];
+
 void Init_ISR(){
   cli();
 
@@ -16,22 +20,22 @@ void Init_ISR(){
   TCCR2B = 0;
 
   // Timer Interrupt Mask Register
-  TIMSK2 |= (1 << OCIE2A);  // Timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);            // Timer compare interrupt
 
   // Timer Interrupt Flag Register
   TIFR2 |= (0 << TOV2);
 
   // Clock source
-  TCCR2B |= (1 << CS21);  // Clk / 8 from prescaler
-  TCCR2B |= (1 << WGM22); //| (1 << WGM13)
+  TCCR2B |= (1 << CS21);              // Clk / 8 from prescaler
+  TCCR2B |= (1 << WGM22);             //| (1 << WGM13)
 
   // // Set pin toggle for Channel A of timer 1 and 2
   // TCCR1A |= (1 << COM1A0);     
-  // TCCR2A |= (1 << COM2A0);       // normal operation - external pins disconnected
+  // TCCR2A |= (1 << COM2A0);         // normal operation - external pins disconnected
 
   uint16_t reg_value = (uint16_t) (F_OSC / (16 * F_PULSE)) - 1;
 
-  OCR2A = reg_value;                // This will generate 250kHz clock
+  OCR2A = reg_value;                  // This will generate 250kHz clock
 
   sei();
 }
@@ -62,6 +66,16 @@ ISR(TIMER2_COMPA_vect)
 
 Stepper::Stepper()
 {
+
+  if (stepperCount < MAX_STEPPERS){
+    this->stepperIndex = stepperCount++;
+    
+  }
+  else{
+    this->stepperIndex = INVALID_STEPPER;
+  }
+
+
   Init_ISR();
 }
 
--- card_robot_controller/tb6600_stepper.h
+++ card_robot_controller/tb6600_stepper.h
@@ -13,6 +13,14 @@ Runs using Timer 2
 #define F_PULSE 250000         // 4us pulses
 #define TIMER4_PRE_SCALER 8
 
+#define MAX_STEPPERS 5
+#define INVALID_STEPPER 255
+
+typedef struct{
+  uint8_t stepper_pin;
+  volatile uint16_t pulses;
+} stepper_t;
+
 class Stepper
 {
   public:
@@ -20,9 +28,10 @@ class Stepper
   void attach(uint8_t pin);
   void set_speed(uint16_t position);
   void move_absolute(uint16_t distance);
-  void move_relative(uint16_t distance)
+  void move_relative(uint16_t distance);
 
   private:
+  uint8_t stepperIndex;
 
 };
 
